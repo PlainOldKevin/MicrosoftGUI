5c46553eb5c097cf0e337db4cf6bcdfecdfd1cc6
GregjQU <gjeanbaptiste@quinnipiac.edu>
2022-05-01 17:00:21 -0400

    trying to make proper set up

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,24 @@
-module MicrosoftGUI {
+import java.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JPanel;
+module MicrosoftGUI extends JPanel {
 
 System.out.println("Welcome! this is a proto type ");
+
+public MicrosoftGUI(){
+
+
+}
+
+
 
 }
