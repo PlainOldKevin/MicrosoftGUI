2108052f80b1fe48d440a389b47056814a73fecf
GregjQU <gjeanbaptiste@quinnipiac.edu>
2022-05-02 11:57:14 -0400

    creating classes to add images as sprites

---
+++ src/App.java
@@ -0,0 +1,28 @@
+
+import java.awt.BorderLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class App extends JFrame {
+
+	public App() {
+		
+		super("Microsoft Demo");
+		this.setLayout(new BorderLayout());
+		
+		Pannel drawingPanel = new Pannel();
+		
+	}
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+new App();
+	}
+
+}
---
+++ src/Pannel.java
@@ -0,0 +1,28 @@
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+
+import javax.swing.JPanel;
+
+public class Pannel extends JPanel {
+
+	private Sprites QULogo;
+	private Sprites MicrosoftLogo;
+	
+	
+	public Pannel() {
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
